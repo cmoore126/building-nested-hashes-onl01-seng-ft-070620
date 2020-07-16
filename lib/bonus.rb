ee163f95bc62fb6089f4fd84994646bbfef780c5
Clayton Moore <cmoorefour@gmail.com>
2020-07-16 20:48:50 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -7,7 +7,7 @@ def bonus
    :hero_friends => [{name:"Benvolio" ,age:"17" ,attitude:"worried"  }, {name:"Mercutio" ,age:"18" ,attitude:"hot-headed"}]},
 
 
-:capulet => { :capulet => {
+{ :capulet => {
   {:patriarch => {name: "Lord Capulet", age: "50"},
   :matriarch => {name: "Lady Capulet", age: "51"},
   :heroine => {name: "Juliet", age: "15", status: "live"},
