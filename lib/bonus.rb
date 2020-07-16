664e7c22231317ca6402ab775d880232af0c3f31
Clayton Moore <cmoorefour@gmail.com>
2020-07-16 20:30:57 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -12,10 +12,9 @@ def bonus
   :matriarch => {name: "Lady Capulet", age: "51"},
   :heroine => {name: "Juliet", age: "15", status: "live"},
   :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},{name: "Nurse", age: "44", attitude: "worried"}]} #your hashes here!
+  }
  }
 }
-
-
   #code your solution here:
 
 epic_tragedy[:montague][:hero][:status] = "dead"
