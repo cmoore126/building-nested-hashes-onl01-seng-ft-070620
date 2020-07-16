400fbda336c6e5f59dbedc23611a622d678ac6a6
Clayton Moore <cmoorefour@gmail.com>
2020-07-16 19:48:45 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -10,9 +10,9 @@ def bonus
   :patriarch => {name: "Lord Capulet", age: "50"},
   :matriarch => {name: "Lady Capulet", age: "51"},
   :heroine => {name: "Juliet", age: "15", status: "dead"},
-  :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},{name: "Nurse", age: "44", attitude: "worried"}  #your hashes here!
-       ]
-    }}
+  :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},{name: "Nurse", age: "44", attitude: "worried"}]} #your hashes here!
+
+     }
 
    }
 
