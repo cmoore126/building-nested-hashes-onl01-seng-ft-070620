fa850582e3d9fe036f2e241e253a1a3c4fa918ee
Clayton Moore <cmoorefour@gmail.com>
2020-07-16 20:49:36 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -7,14 +7,14 @@ def bonus
    :hero_friends => [{name:"Benvolio" ,age:"17" ,attitude:"worried"  }, {name:"Mercutio" ,age:"18" ,attitude:"hot-headed"}]},
 
 
- :capulet => 
+ :capulet =>
   {:patriarch => {name: "Lord Capulet", age: "50"},
   :matriarch => {name: "Lady Capulet", age: "51"},
   :heroine => {name: "Juliet", age: "15", status: "live"},
   :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},{name: "Nurse", age: "44", attitude: "worried"}] #your hashes here!
   }
  }
-}
+
   #code your solution here:
 
 epic_tragedy[:montague][:hero][:status] = "dead"
